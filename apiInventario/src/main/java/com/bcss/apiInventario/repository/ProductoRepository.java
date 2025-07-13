e452234422d4de4ecb46d828daae416d03532059
BenyiUriel <benyi.uriel2006@gmail.com>
2025-07-13 14:50:16 -0600

    Se crea el ProductoRepository

---
+++ apiInventario/src/main/java/com/bcss/apiInventario/repository/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.bcss.apiInventario.repository;
+
+import com.bcss.apiInventario.domain.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+}
